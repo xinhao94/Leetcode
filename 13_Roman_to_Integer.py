7339269d1f80234273fb220d2418314f3959dc89
Xinhao Song <songxinhao@hotmail.com>
2019-05-07 15:44:43 -0500

    Update 13_Roman_to_Integer.py

--- 13_Roman_to_Integer.py
+++ 13_Roman_to_Integer.py
@@ -10,8 +10,12 @@ class Solution:
         
         num = list(s)
         l = len(num)
+        #Initialize the final result to zero
         result = 0
         
+        #Compare each character in Roman from left to right
+        #If it is greater than or equal to the left, it should be added
+        #Otherwise it should be subtracted
         for i in range(l-1):
             if rule[num[i]]>=rule[num[i+1]]:
                 result += rule[num[i]]
