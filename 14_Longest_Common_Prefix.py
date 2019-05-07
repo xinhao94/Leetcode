fa10336846cefc0e3069d91b307d5f31630eda54
Xinhao Song <songxinhao@hotmail.com>
2019-05-07 15:40:53 -0500

    Update 14_Longest_Common_Prefix.py

--- 14_Longest_Common_Prefix.py
+++ 14_Longest_Common_Prefix.py
@@ -1,19 +1,24 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        #If input string is empty, return empty string
         if strs == []:
             return ""
         num_str = len(strs)
+        #Get the shortest string in the list, the length of which cannot be exceeded by common prefix
         min_char = len(min(strs, key=len))
+        #"j" records the length of the prefix
         j = 0
         
         while j < min_char:
             i = 0
+            #Compare the first character of each string
             while i < (num_str-1):
                 if strs[i][j] == strs[i+1][j]:
                     i += 1
                     continue
                 else:
                     break
+            #If the first character of all strings is the same, compare the next
             if i == (num_str-1):
                 j += 1
                 continue
