8c5f56545b2da6533d0aee27df7f4596066edef0
Xinhao Song <songxinhao@hotmail.com>
2019-06-13 13:25:51 -0500

    Update 0058_Length_of_Last_Word.py

--- 0058_Length_of_Last_Word.py
+++ 0058_Length_of_Last_Word.py
@@ -7,8 +7,14 @@ class Solution:
         else:
             i = -1
             count = 0
+            #If there are space characters at the end of the string,
+            #move the probe until it reaches a real character or
+            #reaches the begining of the string
             while i >= -len(s) and s[i] == ' ':
                 i -= 1
+            #Record the length of last word in variable "count" until
+            #the probe reaches another space character or reaches
+            #the begining of the string
             while i >= -len(s) and s[i] != ' ':
                 i -= 1
                 count += 1
